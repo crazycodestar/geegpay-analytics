a882b8f5919c98710cb745d6de3398a904bee564
olalekan <olamilekanadekanmbi@gmail.com>
2024-01-26 18:07:47 +0100

    fixed errors

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,12 +6,11 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatDate(date: Date) {
-	const options = {
+	return date.toLocaleDateString("en-US", {
 		year: "numeric",
 		month: "long",
 		day: "numeric",
-	};
-	return date.toLocaleDateString("en-US", options);
+	});
 }
 
 export function getInitials(name: string) {
